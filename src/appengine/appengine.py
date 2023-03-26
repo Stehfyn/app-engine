7693ce0a8a8c1a7e4cca762866fd171751f435c7
Stephen Foster <stephenfostercpp@gmail.com>
2023-03-25 19:37:15 -0700

    Start of generalizing

--- src/appengine/appengine.py
+++ src/appengine/appengine.py
@@ -1,8 +1,16 @@
-from imgui_bundle import imgui, hello_imgui, immapp
+from imgui_bundle import imgui, hello_imgui, immapp, glfw_utils
 from imgui_bundle.immapp import static
+from enum import Flag
+import glfw
 
-def go():
-    app = __App()
+
+class AppFlags(Flag):
+    MonitorDockspace = (1 << 0)
+    CustomTitlebar = (1 << 1)
+
+
+def go(flags: AppFlags) -> int:
+    app = __App(flags)
     try:
         with app:
             app.run()
@@ -12,96 +20,11 @@ def go():
 
     return 0
 
+
 class __App:
-    def __init__(self):
-
+    def __init__(self, flags: AppFlags) -> None:
         self.runner_params = hello_imgui.RunnerParams()
-        self.runner_params.app_window_params.window_title = "Dear ImGui Bundle interactive manual"
-
-        self.runner_params.app_window_params.hidden = True
-
-        self.runner_params.imgui_window_params.show_menu_bar = True
-        self.runner_params.imgui_window_params.show_status_bar = True
-        self.runner_params.imgui_window_params.config_windows_move_from_title_bar_only = False
-
-        self.runner_params.imgui_window_params.default_imgui_window_type = (
-            hello_imgui.DefaultImGuiWindowType.no_default_window
-        )
-
-        self.runner_params.imgui_window_params.enable_viewports = True
-
-        def init():
-            imgui.get_io().config_viewports_no_auto_merge = True
-            self.logo_tex = hello_imgui.im_texture_id_from_asset("images/UNR-active.png")
-            self.close_tex = hello_imgui.im_texture_id_from_asset("images/close.png")
-
-        @static(init=True, sel=False)
-        def show_gui():
-            imgui.show_style_editor()
-
-            imgui.begin("hi", None, imgui.WindowFlags_.menu_bar | imgui.WindowFlags_.no_title_bar | imgui.WindowFlags_.no_docking | imgui.WindowFlags_.no_bring_to_front_on_focus)
-            if show_gui.init:
-                imgui.set_window_size(imgui.ImVec2(400, 60))
-                show_gui.init = False
-            #imgui.push_style_var(imgui.StyleVar_.frame_padding, imgui.ImVec2(0, 10))
-
-            if (imgui.internal.begin_viewport_side_bar("MainMenuBar", imgui.get_window_viewport(), imgui.Dir_.up,
-                                                       imgui.get_frame_height(),
-                                                       imgui.WindowFlags_.menu_bar | imgui.WindowFlags_.no_title_bar | imgui.WindowFlags_.no_docking | imgui.WindowFlags_.no_saved_settings)):
-                imgui.push_clip_rect(imgui.ImVec2(imgui.get_window_width(), imgui.get_frame_height()), imgui.ImVec2(imgui.get_window_width(), imgui.get_frame_height() * 2), True)
-                #imgui.pop_style_var(1)
-                if (imgui.begin_menu_bar()):
-                    start = imgui.get_cursor_screen_pos()
-                    end = imgui.ImVec2(start.x + imgui.get_frame_height(), start.y + imgui.get_frame_height())
-                    imgui.get_window_draw_list().add_image(self.logo_tex, start, end)
-                    imgui.dummy(imgui.ImVec2(imgui.get_frame_height(), imgui.get_frame_height()))
-
-                    if (imgui.begin_popup_context_item("App Context Menu")):
-                        if (imgui.selectable("Restore", show_gui.sel)):
-                            pass
-                        if (imgui.selectable("Move", show_gui.sel)):
-                            pass
-                        if (imgui.selectable("Size", show_gui.sel)):
-                            pass
-                        if (imgui.selectable("Minimize", show_gui.sel)):
-                            pass
-                        clicked, val = imgui.selectable("Close", show_gui.sel)
-                        if clicked:
-                            hello_imgui.get_runner_params().app_shall_exit = True
-                        imgui.end_popup()
-                    imgui.open_popup_on_item_click("App Context Menu", 0)
-
-                    if (imgui.is_item_hovered() and imgui.is_mouse_double_clicked(0)):
-                        self.runner_params.app_shall_exit = True
-                    if (imgui.begin_menu(("File"))):
-                        imgui.end_menu()
-                    if (imgui.begin_menu(("Edit"))):
-                        imgui.end_menu()
-                    if (imgui.begin_menu(("Select"))):
-                        imgui.end_menu()
-
-                    imgui.push_item_width(120)
-                    imgui.input_text_with_hint("##search", "Search (Ctrl+Q)", "",)
-                    imgui.pop_item_width()
-
-                    start = imgui.get_cursor_pos()
-                    start.x = imgui.get_window_width() - imgui.get_frame_height() - imgui.get_style().frame_padding.x
-                    imgui.set_cursor_pos(start)
-                    #imgui.push_style_var(imgui.StyleVar_.frame_padding, imgui.ImVec2(0,0))
-                    padding = imgui.get_style().frame_padding
-                    if (imgui.image_button("", self.close_tex, imgui.ImVec2(imgui.get_frame_height() - padding.x, imgui.get_frame_height() - padding.x))):
-                        hello_imgui.get_runner_params().app_shall_exit = True
-                    #imgui.pop_style_var(1)
-                    imgui.end_menu_bar()
-
-                    imgui.pop_clip_rect()
-
-                    imgui.end()
-
-            imgui.end()
-
-        self.runner_params.callbacks.post_init = init
-        self.runner_params.callbacks.show_gui = show_gui
+        self.flags = flags
 
     def __enter__(self):
         pass
@@ -110,4 +33,5 @@ class __App:
         pass
 
     def run(self):
-        immapp.run(runner_params=self.runner_params)
+        pass
+
--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 import sys
 import appengine.appengine as ae
 
+
 def main(arg, argv):
-    return ae.go()
+    return ae.go(ae.AppFlags.MonitorDockspace | ae.AppFlags.CustomTitlebar)
+
 
 if __name__=="__main__":
     sys.exit(main(len(sys.argv), sys.argv))
